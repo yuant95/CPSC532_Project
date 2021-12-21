9d54fead3fadb311d73f70d533b2ffdd20458d20
Lironne <lironkurz@gmail>
2021-12-20 19:28:47 -0800

    model 1 working

--- MCMC.py
+++ MCMC.py
@@ -1,18 +1,8 @@
 # Copyright Contributors to the Pyro project.
 # SPDX-License-Identifier: Apache-2.0
 
-"""
-Example: Bayesian Neural Network
-================================
-We demonstrate how to use NUTS to do inference on a simple (small)
-Bayesian neural network with two hidden layers.
-.. image:: ../_static/img/examples/bnn.png
-    :align: center
-"""
-
-import argparse
-import os
-import time
+import argparse, os, time 
+import utils 
 
 import matplotlib
 import matplotlib.pyplot as plt
@@ -44,23 +34,23 @@ def nonlin(x):
 # a two-layer bayesian neural network with computational flow
 # given by D_X => D_H => D_H => D_Y where D_H is the number of
 # hidden units. (note we indicate tensor dimensions in the comments)
-def model(X, Y, D_H, D_Y):
+def model(X, Y, D_H, D_Y, i_prior,o_prior,ps_prior):
     N, D_X = X.shape
 
     # sample first layer (we put unit normal priors on all weights)
-    w1 = numpyro.sample("w1", dist.Normal(jnp.zeros((D_X, D_H)), jnp.ones((D_X, D_H))))
+    w1 = numpyro.sample("w1", dist.Normal(jnp.array(i_prior), jnp.ones((D_X, D_H))))
     assert w1.shape == (D_X, D_H)
     z1 = nonlin(jnp.matmul(X, w1))  # <= first layer of activations
     assert z1.shape == (N, D_H)
 
     # sample second layer
-    w2 = numpyro.sample("w2", dist.Normal(jnp.zeros((D_H, D_H)), jnp.ones((D_H, D_H))))
+    w2 = numpyro.sample("w2", dist.Normal(jnp.array(o_prior), jnp.ones((D_H, D_H))))
     assert w2.shape == (D_H, D_H)
     z2 = nonlin(jnp.matmul(z1, w2))  # <= second layer of activations
     assert z2.shape == (N, D_H)
 
     # sample final layer of weights and neural network output
-    w3 = numpyro.sample("w3", dist.Normal(jnp.zeros((D_H, D_Y)), jnp.ones((D_H, D_Y))))
+    w3 = numpyro.sample("w3", dist.Normal(jnp.array(ps_prior), jnp.ones((D_H, D_Y))))
     assert w3.shape == (D_H, D_Y)
     z3 = jnp.matmul(z2, w3)  # <= output of the neural network
     assert z3.shape == (N, D_Y)
@@ -79,53 +69,23 @@ def model(X, Y, D_H, D_Y):
 
 
 # helper function for HMC inference
-def run_inference(model, args, rng_key, X, Y, D_H, D_Y):
+def run_inference(model, config, rng_key, X, Y, D_H, D_Y, i_prior,o_prior, ps_prior):
     start = time.time()
     kernel = NUTS(model)
-    mcmc = MCMC(
-        kernel,
-        num_warmup=args.num_warmup,
-        num_samples=args.num_samples,
-        num_chains=args.num_chains,
-        progress_bar=False if "NUMPYRO_SPHINXBUILD" in os.environ else True,
-    )
-    mcmc.run(rng_key, X, Y, D_H, D_Y)
+    mcmc = MCMC(kernel,num_warmup=config['num_warmup'],num_samples=config['num_samples'],
+        num_chains=config['num_chains'], progress_bar=False if "NUMPYRO_SPHINXBUILD" in os.environ else True,)
+    mcmc.run(rng_key, X, Y, D_H, D_Y, i_prior,o_prior, ps_prior)
     mcmc.print_summary()
     print("\nMCMC elapsed time:", time.time() - start)
     return mcmc.get_samples()
 
 
 # helper function for prediction
-def predict(model, rng_key, samples, X, D_H, D_Y):
+def predict(model, rng_key, samples, X, D_H, D_Y, i_prior,o_prior, ps_prior):
     model = handlers.substitute(handlers.seed(model, rng_key), samples)
     # note that Y will be sampled in the model because we pass Y=None here
     model_trace = handlers.trace(model).get_trace(X=X, Y=None, D_H=D_H, D_Y = D_Y)
     return model_trace["Y"]["value"]
-
-# Note: Current it's using test 1 as training, and test 2 as test data
-# Only using ROBOT_POSE_DATA_ITEMS and CONTROLLER_DATA_ITEMS
-def read_data(N=100, N_test=10):
-    # To get rid of some noisy set up phase data
-    offset = 200
-    N += offset
-    finalizedDataPath = os.path.join(DRIVE_TEST_1_FOLDER, "finalizedData2.csv")
-    testDataPath = os.path.join(DRIVE_TEST_2_FOLDER, "finalizedData.csv")
-    data = pd.read_csv(finalizedDataPath)
-    testData = pd.read_csv(testDataPath)
-
-    # Normalize the data
-    scaler = MinMaxScaler()
-
-    data[data.columns] = scaler.fit_transform(data)
-    testData[testData.columns] = scaler.fit_transform(testData)
-
-    X = jnp.array(data.loc[offset:N-1, ROBOT_POSE_DATA_ITEMS+CONTROLLER_DATA_ITEMS].to_numpy())
-    Y = jnp.array(data.loc[offset+1:N,ROBOT_POSE_DATA_ITEMS].to_numpy())
-
-    X_test = jnp.array(testData.loc[offset:offset+N_test, ROBOT_POSE_DATA_ITEMS+CONTROLLER_DATA_ITEMS].to_numpy())
-    Y_test_truth = jnp.array(testData.loc[offset+1:offset+N_test+1, ROBOT_POSE_DATA_ITEMS].to_numpy())
-
-    return X, Y, X_test, Y_test_truth
 
 def plot_result(X,Y,X_test,predictions, xDim, yDim, prefix = ""):
     xName = (ROBOT_POSE_DATA_ITEMS+CONTROLLER_DATA_ITEMS)[xDim]
@@ -164,24 +124,35 @@ def plot_result(X,Y,X_test,predictions, xDim, yDim, prefix = ""):
     plt.savefig("{}_data_x{}_y{}.png".format(prefix, xName, yName))
     plt.close(fig)
 
-def main(args):
-    D_Y = 4
-    N_test = 1000
-    N, D_X, D_H = args.num_data, D_Y+2, args.num_hidden
-    
-    X, Y, X_test, Y_test_truth = read_data(N, N_test)
+def run_sampler(config, data, N, i_prior, o_prior, ps_prior):
+
+    numpyro.set_platform(config['mcmc']['device'])
+    numpyro.set_host_device_count(config['mcmc']['num_chains'])
+
+    D_X, D_H = 20, 50 
+
+    values = data
+    D_Y = 18
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled = scaler.fit_transform(values)
+    reframed = utils.series_to_supervised(scaled, 1, 1)
+    reframed.drop(reframed.columns[[20, 21]], axis=1, inplace=True)
+
+    train_X, train_y, test_X, test_y = utils.prep_data_mcmc(reframed, 18)
 
     # do inference
     rng_key, rng_key_predict = random.split(random.PRNGKey(0))
-    samples = run_inference(model, args, rng_key, X, Y, D_H, D_Y)
+    samples = run_inference(model, config['mcmc'], rng_key, train_X, train_y, D_H, D_Y, i_prior,o_prior, ps_prior)
+
+    print('samples? ', samples)
 
     # predict Y_test at inputs X_test
     vmap_args = (
         samples,
-        random.split(rng_key_predict, args.num_samples * args.num_chains),
+        random.split(rng_key_predict, config['mcmc']['num_samples'] * config['mcmc']['num_chains']),
     )
     predictions = vmap(
-        lambda samples, rng_key: predict(model, rng_key, samples, X_test, D_H, D_Y)
+        lambda samples, rng_key: predict(model, rng_key, samples, test_X, D_H, D_Y, i_prior,o_prior, ps_prior)
     )(*vmap_args)
 
     mean_prediction = jnp.mean(predictions, axis=0)
@@ -191,18 +162,18 @@ def main(args):
         json.dump(np.array(mean_prediction).tolist(), f)
 
     with open("groundTruth.json", "w") as f:
-        json.dump(np.array(Y_test_truth).tolist(), f)
+        json.dump(np.array(test_y).tolist(), f)
 
     for i in range(D_X):
         for j in range(D_Y):
             prefix = "{}data_{}hidden_".format(N, D_H)
-            plot_result(X,Y,X_test,predictions,i, j, prefix)
+            plot_result(train_X,train_y,test_X,predictions,i, j, prefix)
 
     # Plot trajectory comparison figure
     fig, ax = plt.subplots(figsize=(8, 8), constrained_layout=True)
 
     # plot ground truth
-    ax.plot(Y_test_truth[:,-4], Y_test_truth[:,-3], "kx")
+    ax.plot(test_y[:,-4], test_y[:,-3], "kx")
 
     # plot prediction
     ax.plot(mean_prediction[:,-4], mean_prediction[:,-3], "bo")
@@ -210,20 +181,3 @@ def main(args):
     ax.set(xlabel="X_{}".format("pose.position.x"), ylabel="Y_{}".format("pose.position.y"), title="Mean predictions with 90% CI")
 
     plt.savefig("{}_comparison.png".format(prefix))
-
-
-if __name__ == "__main__":
-    assert numpyro.__version__.startswith("0.8.0")
-    parser = argparse.ArgumentParser(description="Bayesian neural network example")
-    parser.add_argument("-n", "--num-samples", nargs="?", default=2000, type=int)
-    parser.add_argument("--num-warmup", nargs="?", default=1000, type=int)
-    parser.add_argument("--num-chains", nargs="?", default=1, type=int)
-    parser.add_argument("--num-data", nargs="?", default=4000, type=int)
-    parser.add_argument("--num-hidden", nargs="?", default=7, type=int)
-    parser.add_argument("--device", default="cpu", type=str, help='use "cpu" or "gpu".')
-    args = parser.parse_args()
-
-    numpyro.set_platform(args.device)
-    numpyro.set_host_device_count(args.num_chains)
-
-    main(args)
